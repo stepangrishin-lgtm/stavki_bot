9d93185099d0d88cd597e1595ed80e661c486675
stepangrishin-lgtm <stepangrishin@gmail.com>
2026-02-01 11:03:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -658,7 +658,9 @@ async def on_text(m: Message):
 # ---------------- MAIN ----------------
 async def main():
     await db_init()
+    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
+
